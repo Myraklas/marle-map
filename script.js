b3123ee648b4ab78a797119765b9f2459dcbf533
Myraklas <lukas@berlin20.de>
2025-08-29 19:02:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,22 +8,19 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   // <-- HIER deine finale Bildgröße eintragen (in Pixeln)
-  const imageWidth = 6000;  // z.B. 6000
-  const imageHeight = 4000; // z.B. 4000
+  const imageWidth = 14400;  // z.B. 6000
+  const imageHeight = 12501; // z.B. 4000
 
   // Bild-Bounds [ [minY,minX], [maxY,maxX] ] im Pixelraum
   const bounds = [[0, 0], [imageHeight, imageWidth]];
 
   // <-- HIER den Dateinamen deines Bildes anpassen
-  const image = L.imageOverlay("marle-map.png", bounds).addTo(map);
+  const image = L.imageOverlay("Marle-Map.jpg", bounds).addTo(map);
 
   // Auf volle Karte zoomen
   map.fitBounds(bounds);
 
-  // Beispielmarker – später löschen/ersetzen
-  L.marker([imageHeight * 0.38, imageWidth * 0.5]).addTo(map)
-    .bindPopup("Valmorra (Beispiel)")
-    .openPopup();
+
 
   // Optional: Rechteck-Zoom per Shift+Drag aktivieren (ist in Leaflet Standard)
   // Optional: Doppelklick zum Zoomen deaktivieren, wenn es nervt:
