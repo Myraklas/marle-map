5a9e94fc758bea4bc7362ac9e6e89fb8b5254e5d
Myraklas <lukas@berlin20.de>
2025-08-29 19:43:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -25,9 +25,27 @@ document.addEventListener("DOMContentLoaded", () => {
   // Jetzt erst Zoom-Grenzen und Startzoom setzen
   map.setMinZoom(-6);   // sehr weit raus; bei Bedarf -12/-14 versuchen
   map.setMaxZoom(6);     // ordentlich rein
-  map.setZoom (-4)
+  map.setZoom(-4);
 
   // Beispielmarker (löschen/ersetzen)
   L.marker([imageHeight * 0.5, imageWidth * 0.5]).addTo(map)
     .bindPopup("Mitte der Karte");
+
+
+  // Beispiel: Polygon für Valmorra
+const valmorra = [
+  [4000, 3000],
+  [4200, 3600],
+  [4600, 3400],
+  [4400, 2800]
+];
+
+L.polygon(valmorra, {
+  color: "red",
+  weight: 2,
+  fillColor: "red",
+  fillOpacity: 0.25
+}).addTo(map)
+  .bindPopup("<h3>Valmorra</h3><p>Das Ritterreich</p>");
+
 });
