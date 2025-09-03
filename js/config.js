046d5a65ad574bb8ecb124f8ac09e85f4e791da2
Myraklas <lukas@berlin20.de>
2025-09-03 10:19:10 +0200

    Add section and property comments to config

--- js/config.js
+++ js/config.js
@@ -1,21 +1,30 @@
 window.MARLE_CONFIG = {
+  // Einstellungen für das Kartenbild
   image: {
-    // Pfad ins assets/-Verzeichnis
+    // Dateipfad zum Kartenbild
     name: "assets/Marle-Map.jpg",
+    // Bildbreite in Pixeln
     width: 14400,
+    // Bildhöhe in Pixeln
     height: 12501,
   },
+  // Einstellungen zum Zoomverhalten
   zoom: {
-    minExtra: -6,    // wie weit zusätzlich raus
-    maxExtra: +6,    // wie weit zusätzlich rein
-    startOffset: -3 // so belassen, so ist sehr gut
+    // Wie weit zusätzlich herausgezoomt werden kann
+    minExtra: -6,
+    // Wie weit zusätzlich hineingezoomt werden kann
+    maxExtra: +6,
+    // Startversatz des Zoomfaktors
+    startOffset: -3
   },
+  // Einstellungen für die Anzeige von Orten
   places: {
     // Ab welcher Zoomstufe Orte angezeigt werden (relativ zu fitBounds-Zoom)
     minExtra: -2.5
   },
+  // Verweise auf Datenquellen
   data: {
-    // Pfad ins data/-Verzeichnis
+    // Pfad zur Nations-JSON-Datei
     nationsUrl: "data/nations/index.json?v=1"
   }
 };
